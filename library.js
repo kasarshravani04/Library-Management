3c870919f69bd0e41d9f3a5e83e6aa8e18b7d0a4
kasarshravani04 <shravanikasar05@gmail.com>
2025-10-01 15:38:03 +0530

    added js

--- library.js
+++ library.js
@@ -1,1 +1,25 @@
 import{book} from "./book.js";
+export class library{
+    constructor(name,location){
+        this.name = name,
+        this.location = location,
+        this.booklist = [];
+    }
+   /*
+    @params => book (object)
+  */
+
+addbook(book){
+    const{title,author,genre,isbn} = book;
+    if(!title && !author && !genre && !isbn){
+        return alert("details missing");
+    }
+    this.booklist.push(book);
+}  
+
+showBookList() {
+    console.log(this.bookList);
+  }
+}
+
+
