cae7c63422427be1b30f458b61c611220b6a55cc
kasarshravani04 <shravanikasar05@gmail.com>
2025-10-01 15:48:37 +0530

    adding files

--- app.js
+++ app.js
@@ -6,5 +6,30 @@ import{Library} from "./library.js";
 const add_book_form = document.getElementById(
     "add_book_form"
 )
-const mylib = new Library("My Library","Night City");
+const myLib = new Library("My Library","Night City");
 
+// on submit function
+
+function onsubmit(event){
+    event.preventDefault();
+    let bookTitle = event.target[0].value;
+    let bookAuthor = event.target[1].value;
+    let bookGenre = event.target[2].value;
+    let bookIsbn = event.target[3].value;
+    let bookPublicationYear = event.target[4].value;
+    let bookQuantity = event.target[5].value;
+
+
+const newBook = new book(
+   bookTitle,
+   bookAuthor,
+   bookGenre,
+   bookIsbn,
+   bookPublicationYear,
+   bookQuantity, 
+);  
+myLib.addBook(newBook);
+myLib.showBookList();
+console.log(myLib);
+
+}
