d0fe82c58433a34c4f1986e3b81e2b7708ce2b3a
kasarshravani04 <shravanikasar05@gmail.com>
2025-10-01 15:24:08 +0530

    add js

---
+++ app.js
@@ -0,0 +1,10 @@
+import {book} from "./book.js";
+import{Library} from "./library.js";
+//target elements
+// 1> form
+
+const add_book_form = document.getElementById(
+    "add_book_form"
+)
+const mylib = new Library("My Library","Night City");
+
---
+++ library.js
@@ -0,0 +1,1 @@
+import{book} from "./book.js";
