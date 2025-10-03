5d9f8f8f78eb6ecedaa1bf6539d5e6a83432d71e
kasarshravani04 <shravanikasar05@gmail.com>
2025-10-03 15:33:46 +0530

    local storage render

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 import{book} from "./book.js";
+const container = document.getElementById("root");
 export class library{
     constructor(name,location){
         this.name = name,
@@ -10,16 +11,29 @@ export class library{
   */
 
 addBook(book){
-    const{title,author,genre,isbn} = book;
+    /*const{title,author,genre,isbn} = book;
     if(!title && !author && !genre && !isbn){
         return alert("details missing");
-    }
+    }*/
     this.booklist.push(book);
-}  
+ 
+    localStorage.setItem(
+    "booklist",
+    JSON.stringify(this.booklist)
+  );
 
-showBookList() {
-    console.log(this.bookList);
+  this.showBookList() ;
+}
+  showBookList(){
+    container.innerHTML= "";
+    this.booklist.forEach((book) =>{
+      let div = document.createElement("div");
+      div.classList = "book__card";
+      let h2 = document.createElement("h2");
+      h2.innerText = book.title;
+      div.appendChild(h2);
+      container.appendChild(div);
+    } )
+
   }
 }
-
-
